1aa65c87e148b37f8330773fe4bf987e0baf167e
Guilherme Kuhn <g.kuhn0@gmail.com>
2017-09-03 17:52:30 -0700

    Update example Django version to 1.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='admin_multiupload',
-      version='1.8',
+      version='1.10',
       description='Provides a interface to multi file upload for django admin.',
       author='Guilherme Kuhn',
       author_email='g.kuhn0@gmail.com',
